5493f6be105c831729b115087257180a56247efe
emmanuelsec50 <mosesemaemanuel@gmail.com>
2026-08-14 22:20:34 +0300

    configured arouter with chi and added a logging middleware

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	server := &http.Server{Addr: ":3000", Handler: http.HandlerFunc(basicHandler)}
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/hello/", basicHandler)
+	server := &http.Server{Addr: ":3000", Handler: router}
 	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Println("Failed to connect to server, ", err)
