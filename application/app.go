4b590dfaa266a41a0823d4849a0eef94fe2a6efc
emmanuelsec50 <mosesemaemanuel@gmail.com>
2026-08-17 16:45:34 +0300

    finally done

--- application/app.go
+++ application/app.go
@@ -12,51 +12,60 @@ import (
 type App struct {
 	router http.Handler
 	rdb    *redis.Client
+	config Config
 }
 
-func New() *App {
+func New(config Config) *App {
 	app := &App{
-		router: loadRoutes(),
-		rdb:    redis.NewClient(&redis.Options{}),
+		rdb: redis.NewClient(&redis.Options{
+			Addr: config.RedisAddress,
+		}),
+		config: config,
 	}
+
+	app.loadRoutes()
+
 	return app
 }
 
 func (a *App) Start(ctx context.Context) error {
 	server := &http.Server{
-		Addr:    ":3000",
+		Addr:    fmt.Sprintf(":%d", a.config.ServerPort),
 		Handler: a.router,
 	}
 
 	err := a.rdb.Ping(ctx).Err()
 	if err != nil {
-		return fmt.Errorf("Failed to start redis: %w", err)
+		return fmt.Errorf("failed to connect to redis: %w", err)
 	}
 
 	defer func() {
 		if err := a.rdb.Close(); err != nil {
-			fmt.Println("Failed to close redis", err)
+			fmt.Println("failed to close redis", err)
 		}
 	}()
 
 	fmt.Println("Starting server")
+
 	ch := make(chan error, 1)
+
 	go func() {
 		err = server.ListenAndServe()
 		if err != nil {
-			ch <- fmt.Errorf("Failed to start server: %w", err)
+			ch <- fmt.Errorf("failed to start server: %w", err)
 		}
 		close(ch)
 	}()
 
 	select {
 	case err = <-ch:
-		return nil
+		return err
 	case <-ctx.Done():
 		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
 		defer cancel()
-		return server.Shutdown(timeout)
 
+		return server.Shutdown(timeout)
 	}
+
 	return nil
 }
--- application/routes.go
+++ application/routes.go
@@ -4,26 +4,35 @@ import (
 	"net/http"
 
 	"github.com/emmanuelsec50/chi-router/handler"
+	"github.com/emmanuelsec50/chi-router/repository/order"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func loadRoutes() *chi.Mux {
+func (a *App) loadRoutes() {
 	router := chi.NewRouter()
+
 	router.Use(middleware.Logger)
+
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusAccepted)
+		w.WriteHeader(http.StatusOK)
 	})
-	router.Route("/orders", loadOrderRoutes)
-	return router
+
+	router.Route("/orders", a.loadOrderRoutes)
+
+	a.router = router
 }
 
-func loadOrderRoutes(router chi.Router) {
-	orderHandler := &handler.Order{}
+func (a *App) loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{
+		Repo: &order.RedisRepo{
+			Client: a.rdb,
+		},
+	}
 
 	router.Post("/", orderHandler.Create)
 	router.Get("/", orderHandler.List)
-	router.Get("/{id}", orderHandler.GetbyID)
+	router.Get("/{id}", orderHandler.GetByID)
 	router.Put("/{id}", orderHandler.UpdateByID)
 	router.Delete("/{id}", orderHandler.DeleteByID)
 }
